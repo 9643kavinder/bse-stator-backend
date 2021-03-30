6aa872f80b79722ad26bf200930fd6fb31dc7c3d
Kavinder Panwar <i-kavinder.panwar@round.glass>
2021-03-31 02:59:37 +0530

    Init Django app

--- backend/settings.py
+++ backend/settings.py
@@ -130,7 +130,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 DISABLE_COLLECTSTATIC=1
-STATIC_URL = '/static/'
+# STATIC_URL = '/static/'
 # STATIC_ROOT = 'staticfiles'
 # STATIC_URL = '/static/'
 #
