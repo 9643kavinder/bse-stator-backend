e897f5738c4f556421a60b2a05edd0780225336a
Kavinder Panwar <i-kavinder.panwar@round.glass>
2021-03-31 16:15:33 +0530

    clean up some extras

--- backend/celery.py
+++ backend/celery.py
@@ -18,26 +18,27 @@ app.autodiscover_tasks()
 
 @app.task(bind=True)
 def wake_up(self):
-    redis_instance = redis.StrictRedis(host=settings.REDIS_HOST,
-                                       port=settings.REDIS_PORT,
-                                       password=settings.REDIS_PASSWORD,
-                                       db=0)
-    req = Request('https://www.bseindia.com/download/BhavCopy/Equity/EQ260321_CSV.zip', headers={'User-Agent': 'Mozilla/5.0'})
-    with ZipFile(BytesIO(urlopen(req).read())) as my_zip_file:
-        for contained_file in my_zip_file.namelist():
-            for line in my_zip_file.open(contained_file).readlines():
-                output = line.decode()
-                lst_info = output.split(',')
-                dct = {
-                    "code": lst_info[0],
-                    "name": lst_info[1],
-                    "open": lst_info[4],
-                    "high": lst_info[5],
-                    "low": lst_info[6],
-                    "close": lst_info[7]
-                }
-                json_dct = json.dumps(dct)
-                redis_instance.set(lst_info[0], json_dct)
+    print("Hello")
+    # redis_instance = redis.StrictRedis(host=settings.REDIS_HOST,
+    #                                    port=settings.REDIS_PORT,
+    #                                    password=settings.REDIS_PASSWORD,
+    #                                    db=0)
+    # req = Request('https://www.bseindia.com/download/BhavCopy/Equity/EQ260321_CSV.zip', headers={'User-Agent': 'Mozilla/5.0'})
+    # with ZipFile(BytesIO(urlopen(req).read())) as my_zip_file:
+    #     for contained_file in my_zip_file.namelist():
+    #         for line in my_zip_file.open(contained_file).readlines():
+    #             output = line.decode()
+    #             lst_info = output.split(',')
+    #             dct = {
+    #                 "code": lst_info[0],
+    #                 "name": lst_info[1],
+    #                 "open": lst_info[4],
+    #                 "high": lst_info[5],
+    #                 "low": lst_info[6],
+    #                 "close": lst_info[7]
+    #             }
+    #             json_dct = json.dumps(dct)
+    #             redis_instance.set(lst_info[0], json_dct)
 
 
 
